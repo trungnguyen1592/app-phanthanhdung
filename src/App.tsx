d29bc11ed13b54905ce463712810c0a04b82ce56
trung1509 <trung15992@gmail.com>
2026-07-14 13:00:05 +0700

    Feat: Reorganize sidebar menu items and update capitalization

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,18 @@ function App() {
         <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
           <div className="sidebar-logo">PHANTHANHDUNG</div>
           <nav className="sidebar-menu">
-            <Link to="/learning" className="menu-item"><LayoutDashboard size={20} /> Hướng dẫn học</Link>
-            <Link to="#" className="menu-item"><BookOpen size={20} /> Bản đồ thành công</Link>
-            <Link to="#" className="menu-item"><Video size={20} /> Short triệu view</Link>
-            <Link to="#" className="menu-item"><Wrench size={20} /> CoachMax 2 kèm 1</Link>
-            <Link to="#" className="menu-item"><Settings size={20} /> Gia hạn Tool</Link>
-            <Link to="#" className="menu-item"><BookOpen size={20} /> Tài nguyên</Link>
-            <Link to="#" className="menu-item"><Users size={20} /> Hồ sơ</Link>
-            <Link to="#" className="menu-item"><Video size={20} /> Share 43 ngách</Link>
+            <Link to="/learning" className="menu-item"><LayoutDashboard size={20} /> Hướng Dẫn Học</Link>
+            <Link to="#" className="menu-item"><BookOpen size={20} /> Khoá Học</Link>
+            <Link to="#" className="menu-item"><BookOpen size={20} /> Bản Đồ Thành Công</Link>
+            <Link to="#" className="menu-item"><Users size={20} /> Quyền Đồng Hành</Link>
+            <Link to="#" className="menu-item"><Video size={20} /> Short Triệu View</Link>
+            <Link to="#" className="menu-item"><Wrench size={20} /> CoachMax 2 Kèm 1</Link>
+            <Link to="#" className="menu-item"><Wrench size={20} /> Tool Vĩnh Viễn</Link>
+            <Link to="#" className="menu-item"><Settings size={20} /> Gia Hạn Tool</Link>
+            <Link to="#" className="menu-item"><BookOpen size={20} /> Tài Nguyên</Link>
+            <Link to="#" className="menu-item"><Video size={20} /> Affiliate</Link>
+            <Link to="#" className="menu-item"><Users size={20} /> Hồ Sơ</Link>
+            <Link to="#" className="menu-item"><Video size={20} /> Share 43 Ngách</Link>
             <Link to="#" className="menu-item"><Wrench size={20} /> Ngách Solo Ultra</Link>
           </nav>
           <div className="sidebar-footer">
