611e5c2e95c0f520be74ec5fdf0a4ebf751d5efe
trung1509 <trung15992@gmail.com>
2026-07-14 12:26:13 +0700

    Feat: Update sidebar menu items

--- src/App.tsx
+++ src/App.tsx
@@ -25,10 +25,15 @@ function App() {
         <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
           <div className="sidebar-logo">PHANTHANHDUNG</div>
           <nav className="sidebar-menu">
-            <Link to="/learning" className="menu-item"><LayoutDashboard size={20} /> Hướng dẫn học</Link>
-            <Link to="#" className="menu-item"><BookOpen size={20} /> Khoá học</Link>
-            <Link to="#" className="menu-item"><Video size={20} /> Affiliate</Link>
-            <Link to="#" className="menu-item"><Wrench size={20} /> Tool vĩnh viễn</Link>
+            <Link to="/learning" className="menu-item"><LayoutDashboard size={20} /> Bản đồ thành công</Link>
+            <Link to="#" className="menu-item"><BookOpen size={20} /> Quyền đồng hành</Link>
+            <Link to="#" className="menu-item"><Video size={20} /> Short triệu view</Link>
+            <Link to="#" className="menu-item"><Wrench size={20} /> CoachMax 2 kèm 1</Link>
+            <Link to="#" className="menu-item"><Settings size={20} /> Gia hạn Tool</Link>
+            <Link to="#" className="menu-item"><BookOpen size={20} /> Tài nguyên</Link>
+            <Link to="#" className="menu-item"><Users size={20} /> Hồ sơ</Link>
+            <Link to="#" className="menu-item"><Video size={20} /> Share 43 ngách</Link>
+            <Link to="#" className="menu-item"><Wrench size={20} /> Ngách Solo Ultra</Link>
           </nav>
           <div className="sidebar-footer">
             <a href="#" className="menu-item"><Settings size={20} /> Cài đặt</a>
